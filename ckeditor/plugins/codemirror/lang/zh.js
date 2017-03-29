01c60d7d287b11215ddd14d1b5401b108eeae730
Jones <Jones@vipcase.net>
2017-03-29 14:06:51 +0800

    編輯器更新

---
+++ ckeditor/plugins/codemirror/lang/zh.js
@@ -0,0 +1,12 @@
+﻿/*
+Copyright (c) 2003-2012, CKSource - Frederico Knabben. All rights reserved.
+For licensing, see LICENSE.html or http://ckeditor.com/license
+*/
+CKEDITOR.plugins.setLang( 'codemirror', 'zh', {
+	toolbar: '原始碼',
+	searchCode: '搜索原始碼',
+	autoFormat: '自動標籤格式化',
+	commentSelectedRange: '加入註解',
+	uncommentSelectedRange: '移除註解',
+	autoCompleteToggle: '開啟/關閉 HTML標籤自動完成'
+});
